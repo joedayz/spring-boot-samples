7d7bcc77121c61dcd9698a2f49a48008da20c11e
Jose Diaz <jose.diaz@joedayz.pe>
2015-07-25 14:02:38 -0500

    Test JMS

--- spring-boot-activemq/src/test/java/demo/DemoActiveMqApplicationTests.java
+++ spring-boot-activemq/src/test/java/demo/DemoActiveMqApplicationTests.java
@@ -1,16 +1,30 @@
 package demo;
 
+import javax.jms.JMSException;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.OutputCapture;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoActiveMqApplication.class)
 public class DemoActiveMqApplicationTests {
 
+	@Rule
+	public OutputCapture outputCapture = 
+				new OutputCapture();
+	
+	@Autowired
+	private Producer producer;
+	
 	@Test
-	public void contextLoads() {
+	public void sendSimpleMessage() throws InterruptedException, JMSException{
+		this.producer.send("Test message");
+		assertTrue(this.outputCapture.toString().contains("Test message"));
 	}
-
 }
