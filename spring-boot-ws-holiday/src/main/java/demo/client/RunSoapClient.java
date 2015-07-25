6a283399a36ef7aeafed48679d51d7b4c1eba58f
Jose Diaz <jose.diaz@joedayz.pe>
2015-07-25 13:49:31 -0500

    Demo Activemq boot

--- spring-boot-ws-holiday/src/main/java/demo/client/RunSoapClient.java
+++ spring-boot-ws-holiday/src/main/java/demo/client/RunSoapClient.java
@@ -1,9 +1,22 @@
 package demo.client;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import pe.joedayz.wsdl.GetStudentResponse;
+import demo.WebServiceConfig;
+
 public class RunSoapClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(WebServiceConfig.class);
+		ctx.refresh();
+		StudentClient studentClient = ctx.getBean(StudentClient.class);
+		System.out.println("For Student Id: 1");
+		GetStudentResponse response = studentClient.getStudentById(1);
+		System.out.println("Name:" + response.getStudent().getName());
+		System.out.println("Age:" + response.getStudent().getAge());
+		System.out.println("Class:" + response.getStudent().getClazz());
 
 	}
 
