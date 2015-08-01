7db5a58494c44a88fee71078ea4334b655a8663e
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-01 13:23:53 -0500

    security config

--- spring-boot-messaging/src/main/java/demo/config/SecurityConfig.java
+++ spring-boot-messaging/src/main/java/demo/config/SecurityConfig.java
@@ -1,11 +1,50 @@
 package demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 /**
  * Created by Celeritech Peru on 01/08/2015.
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // configure a simple in-memory authentication with three users
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("pass1").roles("USER")
+                .and()
+                .withUser("user2").password("pass2").roles("USER")
+                .and()
+                .withUser("user3").password("pass3").roles("USER");
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        // disable CSRF for simplicity and configure a session registry which will allow us to fetch a list of users
+        http.csrf().disable().sessionManagement().maximumSessions(-1).sessionRegistry(sessionRegistry());
+    }
 }
