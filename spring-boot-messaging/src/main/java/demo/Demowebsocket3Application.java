b6cfa0d250bfdb4aa755df4c5c55f5198789e71e
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-01 13:19:15 -0500

    messaging

---
+++ spring-boot-messaging/src/main/java/demo/Demowebsocket3Application.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demowebsocket3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demowebsocket3Application.class, args);
+    }
+}
---
+++ spring-boot-messaging/src/main/java/demo/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Celeritech Peru on 01/08/2015.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
