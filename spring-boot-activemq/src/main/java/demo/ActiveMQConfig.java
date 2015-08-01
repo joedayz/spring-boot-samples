9231e0c58456e974787d8ab6d552c26070398c6b
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-01 12:24:39 -0500

    Simple Demo WebSocket con Boot

--- spring-boot-activemq/src/main/java/demo/ActiveMQConfig.java
+++ spring-boot-activemq/src/main/java/demo/ActiveMQConfig.java
@@ -1,20 +1,34 @@
 package demo;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @EnableJms
 public class ActiveMQConfig {
 
 	@Bean
+	public ConnectionFactory connectionFactory() {
+		return new CachingConnectionFactory(new ActiveMQConnectionFactory(""
+				+ "tcp://localhost:61616"));
+	}
+
+	@Bean
+	JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+		return new JmsTemplate(connectionFactory);
+	}
+
+	@Bean
 	public Queue queue() {
 		return new ActiveMQQueue("sample.queue");
 	}
-	
-	
+
 }
--- spring-boot-activemq/src/main/java/demo/Producer.java
+++ spring-boot-activemq/src/main/java/demo/Producer.java
@@ -4,26 +4,20 @@ import javax.jms.Queue;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Producer implements CommandLineRunner {
+public class Producer  {
 
 	@Autowired
-	private JmsMessagingTemplate jmsMessagingTemplate;
+	private JmsTemplate jmsTemplate;
 
 	@Autowired
 	private Queue queue;
 
-	@Override
-	public void run(String... args) throws Exception {
-		send("Sample message");
-		System.out.println("Message was sent to the Queue");
-	}
-
 	public void send(String msg) {
-		this.jmsMessagingTemplate.convertAndSend(this.queue, msg);
+		this.jmsTemplate.convertAndSend(this.queue, msg);
 	}
 
 }
