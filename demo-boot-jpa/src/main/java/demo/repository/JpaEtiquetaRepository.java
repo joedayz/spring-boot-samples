625917d9efd0d3673bf1c915b2a971d0276caa6c
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-15 13:55:24 -0500

    repositories ready2

--- demo-boot-jpa/src/main/java/demo/repository/JpaEtiquetaRepository.java
+++ demo-boot-jpa/src/main/java/demo/repository/JpaEtiquetaRepository.java
@@ -2,17 +2,26 @@ package demo.repository;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 import demo.domain.Etiqueta;
+import demo.domain.Nota;
 
 @Repository
 public class JpaEtiquetaRepository implements EtiquetaRepository {
 
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	
 	@Override
 	public List<Etiqueta> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.entityManager.createQuery("SELECT e FROM Etiqueta e",Etiqueta.class)
+				.getResultList();
 	}
 
 }
