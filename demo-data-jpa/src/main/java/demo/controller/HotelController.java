5af6ddbe2bf6b9d186a5e136add701cf90eb4ac1
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-22 11:18:42 -0500

    controller

---
+++ demo-data-jpa/src/main/java/demo/controller/HotelController.java
@@ -0,0 +1,26 @@
+package demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo.service.CityService;
+
+@Controller
+public class HotelController {
+
+	@Autowired
+	private CityService cityService;
+	
+	@RequestMapping("/")
+	@ResponseBody
+	@Transactional(readOnly=true)
+	public String hola(){
+		return this.cityService.getCity("Batch", "UK")
+				.getName();
+	}
+	
+	
+}
