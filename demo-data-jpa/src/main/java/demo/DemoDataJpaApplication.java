9809ba73677f5a200557091c2034e660e1afae48
Jose Diaz <jose.diaz@joedayz.pe>
2015-08-22 10:31:44 -0500

    domain ready

---
+++ demo-data-jpa/src/main/java/demo/DemoDataJpaApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDataJpaApplication.class, args);
+    }
+}
