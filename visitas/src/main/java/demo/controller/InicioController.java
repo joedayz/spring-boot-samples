a31f2e8714a013f605f5af679c525461a070d0dc
Jose Amadeo Diaz <jadiaz@farmaciasperuanas.pe>
2019-05-25 14:55:27 -0500

    usando nuevas anotaciones de spring mvc

--- visitas/src/main/java/demo/controller/InicioController.java
+++ visitas/src/main/java/demo/controller/InicioController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import demo.model.Visita;
@@ -21,21 +19,22 @@ public class InicioController {
 
 	@Autowired
 	private VisitasRepository visitasRepository;
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+
+
+	@GetMapping("/")
 	@Transactional(readOnly=true)
-	public ModelAndView inicio(){
-		List<Visita> visitas = (List<Visita>) visitasRepository.findAll();
-		ModelAndView mav = new ModelAndView("home");
-		mav.addObject("visitas",visitas);
-		mav.addObject("visita", new Visita());
-		return mav;
+	public String home(Model model){
+
+		model.addAttribute("visitas", visitasRepository.findAll());
+		model.addAttribute("visita", new Visita());
+		return "home";
 	}
+
 	
-	@RequestMapping(value="/", method=RequestMethod.POST)
-    public String submit(@ModelAttribute Visita visita, 
-    				Model model) {
-        //model.addAttribute("visita", visita);
+
+	@PostMapping("/")
+    public String submit(@ModelAttribute Visita visita) {
+
         visitasRepository.save(visita);
         return "redirect:/";
     }
